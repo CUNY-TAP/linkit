0f8915b08c6673786df5d3480f05c49aab49c3dc
Bedrock02 <JimSteve91@gmal.com>
2013-12-25 20:11:56 -0500

    Added Update Score to Commetns Model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,16 @@
 class Link < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   validates :url, presence: true
+
+
+  #updates cumaltive score
+  def updateScore
+    #find sum of score
+  	updated_score = self.comments.sum(:votes)
+    #update score
+    self.score = updated_score
+    #save update!
+    self.save
+  end
 end
