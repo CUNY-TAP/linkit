09ab9896b58a59e393d9c0c39a4cc9c289ba6914
Angelica <acontero@gmail.com>
2013-12-27 22:50:48 -0500

    added login and logout, simple form which I don't really end up using, and tried to add columns and indexes for user_id in links but that wasn't working

--- linkit/db/schema.rb
+++ linkit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131227190338) do
+ActiveRecord::Schema.define(version: 20131227234435) do
 
   create_table "links", force: true do |t|
     t.string   "url"
