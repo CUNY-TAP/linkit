2965c491345bd5f7e9bf9f03937f5cf19701f3cd
squit <michael.squitieri@gmail.com>
2013-12-27 21:13:40 -0500

    now update link score when comments are deleted

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
 	belongs_to :link
 
 	after_save :update_link_score
+	after_destroy :update_link_score
 
 	validates :user_id, presence: true
 	validates :link_id, presence: true
