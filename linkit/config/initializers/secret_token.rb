37320681be32d110b7151a2d10def23ee2e78e20
Angelica <acontero@gmail.com>
2013-12-27 19:08:42 -0500

    using devise and bootstrap, bit of styling done

---
+++ linkit/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linkit::Application.config.secret_key_base = '6cd95d759550966dba462589c9ec08c1587b3d2689d94086759a615055663c7eb41f61bcf4c600c08c3764a5477fded8abda7cb9f0a0376c96f3cacb905bcc19'
