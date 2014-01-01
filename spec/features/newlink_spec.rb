af31098a8881b461ab23dd984f5a4905b6be3596
Adil Kaouadji <amkaouad@gmail.com>
2014-01-01 15:36:53 -0500

    Link Related Integration Tests Done
    Creating a Link Logged in and Logged out is done

--- spec/features/newlink_spec.rb
+++ spec/features/newlink_spec.rb
@@ -9,3 +9,18 @@ describe "making a new link without being signed in" do
 	expect(page).to have_content "You need to sign in or sign up before continuing."
 end
 
+describe "Making a Link After Logging In"
+user = FactoryGirl.create(:user)
+	visit '/'
+	click_link 'Login'
+	fill_in 'user[Email]', :with => user.email
+	fill_in 'user[Password]', :with => user.password 
+	click_button "Sign In"
+	click_link "New"
+
+	fill_in 'Url', :with => "reddit.com"
+    fill_in 'Name', :with => "reddit"
+    click_button 'Create Link'
+    expect(page).to have_content "Link was successfully created"
+
+end
